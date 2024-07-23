f614131216feecbfba22b2448bf133e4afc7b6db
Pablo <20200135@ricaldone.edu.sv>
2024-07-23 13:52:05 -0600

    primer commit

---
+++ app/src/main/java/pablo/ayala/proyectomodulo202001352B/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package pablo.ayala.proyectomodulo202001352B.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
