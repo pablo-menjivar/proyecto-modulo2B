8bc20875945ffc6c9bc0b48372f24440a749a2b0
Pablo <20200135@ricaldone.edu.sv>
2024-07-23 14:46:14 -0600

    culminando el data class

--- app/src/main/java/pablo/ayala/proyectomodulo202001352B/Modelo/DataClassPacientes.kt
+++ app/src/main/java/pablo/ayala/proyectomodulo202001352B/Modelo/DataClassPacientes.kt
@@ -1,4 +1,16 @@
 package pablo.ayala.proyectomodulo202001352B.Modelo
 
-class DataClassPacientes {
-}
+import java.util.Date
+
+data class pacientes(
+    val uuid: String,
+    var nombre: String,
+    val apellido: String,
+    val edad: Int,
+    val enfermedad: String,
+    val num_habitacion: Int,
+    val num_cama: Int,
+    val medicamento: String,
+    val fecha_ingreso: Date,
+    val hora_aplicacion_medicamentos: String
+    )
