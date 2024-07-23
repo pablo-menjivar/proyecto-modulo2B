1cc23d8d0215ec7d69920bc641dac78282410c60
Pablo <20200135@ricaldone.edu.sv>
2024-07-23 14:42:12 -0600

    culminando la clase conexión

--- app/src/main/java/pablo/ayala/proyectomodulo202001352B/Modelo/ClaseConexion.kt
+++ app/src/main/java/pablo/ayala/proyectomodulo202001352B/Modelo/ClaseConexion.kt
@@ -1,4 +1,20 @@
 package pablo.ayala.proyectomodulo202001352B.Modelo
 
+import java.sql.Connection
+import java.sql.DriverManager
+
 class ClaseConexion {
+    fun cadenaConexion(): Connection? {
+        try {
+            val url = "jdbc:oracle:thin:@192.168.56.1:1521:xe"
+            val usuario = "pbl_43"
+            val contrasena = "98765"
+
+            val connection = DriverManager.getConnection(url, usuario, contrasena)
+            return connection
+        }catch (e: Exception){
+            println("error: $e")
+            return null
+        }
+    }
 }
